7505753a5a6de726cd872644f092a7b2d2480483
Eirik Vullum <evullum@gmail.com>
2014-01-04 20:55:53 +0100

    use dot notation on object parameters

--- test.js
+++ test.js
@@ -4,9 +4,9 @@ var rsaHelpers = require('./main');
 var keys = rsaHelpers.generateKeypair(1000); // generate keys
 //var keys = { public: [ 7, 28417009 ], private: [ 16231783, 28417009 ] }; // ready keypair
 console.log(keys);
-console.log('public key exponent  : ' + keys['public'][0]);
-console.log('private key exponent : ' + keys['private'][0]);
-console.log('modulus              : ' + keys['private'][1]);
+console.log('public key exponent  : ' + keys.private[0]);
+console.log('private key exponent : ' + keys.private[0]);
+console.log('modulus              : ' + keys.public[1]);
 
 var message = 'secret message';
 console.log('Original message     : ' + message);
@@ -14,8 +14,8 @@ console.log('Original message     : ' + message);
 var charMessage = rsaHelpers.toCharArray(message);
 console.log('Converted to integers: ' + charMessage);
 
-var encrypted = rsaHelpers.cryptMessage(keys['public'], message, true);
+var encrypted = rsaHelpers.cryptMessage(keys.public, message, true);
 console.log('Encrypted message    : ' + encrypted);
 
-var decrypted = rsaHelpers.cryptMessage(keys['private'], encrypted, false);
+var decrypted = rsaHelpers.cryptMessage(keys.private, encrypted, false);
 console.log('Decrypted message    : ' + decrypted);
